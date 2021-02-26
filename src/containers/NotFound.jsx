7294017551b3927972d32e4047056153cbfe2559
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2021-02-26 20:26:40 -0300

    Revisando que ruta llega al notfound

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useLocation, withRouter } from 'react-router-dom';
 
-const NotFound = () => (
-  <h1>NotFound</h1>
-);
+class NotFound extends React.Component {
+  componentDidMount() {
+    console.log(this.props);
+  }
+  render() {
+    return <h1>NotFound</h1>;
+  }
+}
 
-export default NotFound;
+// const NotFound = () => (
+//   <h1>NotFound</h1>
+// );
+
+export default withRouter(NotFound);
